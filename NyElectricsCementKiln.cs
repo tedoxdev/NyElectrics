9eda2503b638041eb1080dc5fa1e8ad47d1b6e64
tedoxdev <67103406+tedoxdev@users.noreply.github.com>
2022-03-03 09:38:38 +0100

    Updated for 9.4.6 - part #2

--- NyElectricsCementKiln.cs
+++ NyElectricsCementKiln.cs
@@ -2,137 +2,154 @@
 
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using Eco.Core.Items;
+using Eco.Gameplay.Blocks;
 using Eco.Gameplay.Components;
 using Eco.Gameplay.Components.Auth;
+using Eco.Gameplay.DynamicValues;
+using Eco.Gameplay.Economy;
 using Eco.Gameplay.Housing;
+using Eco.Gameplay.Interactions;
 using Eco.Gameplay.Items;
+using Eco.Gameplay.Modules;
+using Eco.Gameplay.Minimap;
 using Eco.Gameplay.Objects;
+using Eco.Gameplay.Players;
+using Eco.Gameplay.Property;
 using Eco.Gameplay.Skills;
 using Eco.Gameplay.Systems.TextLinks;
 using Eco.Gameplay.Pipes.LiquidComponents;
 using Eco.Gameplay.Pipes.Gases;
-using Eco.Gameplay.Property;
 using Eco.Gameplay.Systems.Tooltip;
+using Eco.Shared;
 using Eco.Shared.Math;
 using Eco.Shared.Localization;
 using Eco.Shared.Serialization;
 using Eco.Shared.Utils;
+using Eco.Shared.View;
+using Eco.Shared.Items;
+using Eco.Gameplay.Pipes;
+using Eco.World.Blocks;
+using Eco.Gameplay.Housing.PropertyValues;
+using static Eco.Gameplay.Housing.PropertyValues.HomeFurnishingValue;
 using Eco.Mods.TechTree;
-using Eco.Core.Plugins.Interfaces;
 
 
 
 namespace NyElectrics
 {
 
-
-
-
-[Serialized]
-[RequireComponent(typeof(ChimneyComponent))]
-[RequireComponent(typeof(LiquidProducerComponent))]
-[RequireComponent(typeof(AttachmentComponent))]
-[RequireComponent(typeof(PropertyAuthComponent))]
-[RequireComponent(typeof(MinimapComponent))]
-[RequireComponent(typeof(LinkComponent))]
-[RequireComponent(typeof(CraftingComponent))]
-[RequireComponent(typeof(PowerGridComponent))]
-[RequireComponent(typeof(PowerConsumptionComponent))]
-[RequireComponent(typeof(HousingComponent))]
-[RequireComponent(typeof(SolidGroundComponent))]
-[RequireComponent(typeof(RoomRequirementsComponent))]
-[RequireRoomContainment]
-[RequireRoomVolume(45)]
-[RequireRoomMaterialTier(1.8f, typeof(CementLavishReqTalent), typeof(CementFrugalReqTalent))]
-public partial class NyElectricCementKilnObject : WorldObject, IRepresentsItem
-{
-  public override LocString DisplayName { get { return Localizer.DoStr("Electric Cement Kiln"); } }
-
-  public virtual Type RepresentedItemType { get { return typeof(NyElectricCementKilnItem); } }
-
-  protected override void Initialize()
-  {
-
-      this.GetComponent<MinimapComponent>().Initialize(Localizer.DoStr("Crafting"));
-      this.GetComponent<HousingComponent>().Set(NyElectricCementKilnItem.HousingVal);
-      this.GetComponent<PowerGridComponent>().Initialize(10, new ElectricPower());
-      this.GetComponent<LiquidProducerComponent>().Setup(typeof(SmogItem), (int)(0.8 * 1000f), this.NamedOccupancyOffset("ChimneyOut"));
-      
-      this.GetComponent<PowerConsumptionComponent>().Initialize(3000f);
-  }
-
-  public override void Destroy()
-  {
-      base.Destroy();
-  }
-
-  static NyElectricCementKilnObject()
-  {
-	WorldObject.AddOccupancy<NyElectricCementKilnObject>(new List<BlockOccupancy>(){new BlockOccupancy(new Vector3i(0, 1, 0), typeof(PipeSlotBlock), new Quaternion(-0.7071068f, 0f, 0f, 0.7071068f), "ChimneyOut"),new BlockOccupancy(new Vector3i(0, 0, 0)),new BlockOccupancy(new Vector3i(1, 0, 0)),new BlockOccupancy(new Vector3i(1, 1, 0)), new BlockOccupancy(new Vector3i(2, 0, 0)),new BlockOccupancy(new Vector3i(2, 1, 0)),new BlockOccupancy(new Vector3i(3, 0, 0)),new BlockOccupancy(new Vector3i(3, 1, 0)),});
-      
-	/* Linked Recipes */
-	CraftingComponent.AddRecipe(typeof(NyElectricCementKilnObject), Recipe.Get<Eco.Mods.TechTree.ReinforcedConcreteRecipe>());
-	CraftingComponent.AddRecipe(typeof(NyElectricCementKilnObject), Recipe.Get<Eco.Mods.TechTree.ConcreteRecipe>());
-	CraftingComponent.AddRecipe(typeof(NyElectricCementKilnObject), Recipe.Get<Eco.Mods.TechTree.BasaltConcreteRecipe>());
-	CraftingComponent.AddRecipe(typeof(NyElectricCementKilnObject), Recipe.Get<Eco.Mods.TechTree.GneissConcreteRecipe>());
-	CraftingComponent.AddRecipe(typeof(NyElectricCementKilnObject), Recipe.Get<Eco.Mods.TechTree.LimestoneConcreteRecipe>());
-	CraftingComponent.AddRecipe(typeof(NyElectricCementKilnObject), Recipe.Get<Eco.Mods.TechTree.ShaleConcreteRecipe>());
-
-  }
-
-}
-
-
-[Serialized]
-public partial class NyElectricCementKilnItem : WorldObjectItem<NyElectricCementKilnObject>
-{
-  public override LocString DisplayName { get { return Localizer.DoStr("Electric Cement Kiln"); } }
-  public override LocString DisplayDescription { get { return Localizer.DoStr("A superior replacement for the cement kiln that use electric power."); } }
-
-  static NyElectricCementKilnItem()
-  {
-
-  }
-
-  [TooltipChildren] public HousingValue HousingTooltip { get { return HousingVal; } }
-  [TooltipChildren]
-  public static HousingValue HousingVal
-  {
-      get
-      {
-          return new HousingValue()
-          {
-              Category = "Industrial",
-              TypeForRoomLimit = "",
-          };
-      }
-  }
-}
-
-[RequiresSkill(typeof(IndustrySkill), 0)]
-public partial class NyElectricCementKilnRecipe : Recipe
-{
-    public NyElectricCementKilnRecipe()
+    [Serialized]
+    [RequireComponent(typeof(AirPollutionComponent))]
+    [RequireComponent(typeof(ChimneyComponent))]
+    [RequireComponent(typeof(OnOffComponent))]
+    [RequireComponent(typeof(LiquidProducerComponent))]
+    [RequireComponent(typeof(AttachmentComponent))]
+    [RequireComponent(typeof(PropertyAuthComponent))]
+    [RequireComponent(typeof(MinimapComponent))]
+    [RequireComponent(typeof(LinkComponent))]
+    [RequireComponent(typeof(CraftingComponent))]
+    [RequireComponent(typeof(PowerGridComponent))]
+    [RequireComponent(typeof(PowerConsumptionComponent))]
+    [RequireComponent(typeof(HousingComponent))]
+    [RequireComponent(typeof(SolidGroundComponent))]
+    [RequireComponent(typeof(PluginModulesComponent))]
+    [RequireComponent(typeof(RoomRequirementsComponent))]
+    [RequireRoomContainment]
+    [RequireRoomVolume(45)]
+    [RequireRoomMaterialTier(1.8f, typeof(MasonryLavishReqTalent), typeof(MasonryFrugalReqTalent))]
+    public partial class NyElectricCementKilnObject : WorldObject, IRepresentsItem
     {
-        this.Products = new CraftingElement[]
+        public override LocString DisplayName { get { return Localizer.DoStr("Electric Cement Kiln"); } }
+
+        public virtual Type RepresentedItemType { get { return typeof(NyElectricCementKilnItem); } }
+
+        protected override void Initialize()
         {
-            new CraftingElement<NyElectricCementKilnItem>(),
-        };
-        this.Ingredients = new CraftingElement[] {
-		new CraftingElement<Eco.Mods.TechTree.CircuitItem>(1f),
-		new CraftingElement<Eco.Mods.TechTree.BrickItem>(typeof(IndustrySkill), 80f, IndustrySkill.MultiplicativeStrategy, typeof(IndustryLavishResourcesTalent)),
-		new CraftingElement<Eco.Mods.TechTree.SteelItem>(typeof(IndustrySkill), 50f, IndustrySkill.MultiplicativeStrategy, typeof(IndustryLavishResourcesTalent)),
 
-	};
+            this.GetComponent<MinimapComponent>().Initialize(Localizer.DoStr("Crafting"));
+            this.GetComponent<PowerGridComponent>().Initialize(10, new ElectricPower());
+            this.GetComponent<PowerConsumptionComponent>().Initialize(3000f);
+            this.GetComponent<HousingComponent>().HomeValue = NyElectricCementKilnItem.HomeValue;
 
-        this.ExperienceOnCraft = 7f;
+            this.GetComponent<LiquidProducerComponent>().Setup(typeof(SmogItem), (int)(0.8 * 1000f), this.NamedOccupancyOffset("ChimneyOut"));
+        }
 
-        this.CraftMinutes = CreateCraftTimeValue(typeof(NyElectricCementKilnRecipe), Item.Get<NyElectricCementKilnItem>().UILink(), 10f, typeof(IndustrySkill), typeof(IndustryFocusedSpeedTalent), typeof(IndustryParallelSpeedTalent));
-        this.Initialize(Localizer.DoStr("Electric Cement Kiln"), typeof(NyElectricCementKilnRecipe));
+        public override void Destroy()
+        {
+            base.Destroy();
+        }
 
-        CraftingComponent.AddRecipe(typeof(ElectricMachinistTableObject), this);
+        static NyElectricCementKilnObject()
+        {
+            WorldObject.AddOccupancy<NyElectricCementKilnObject>(new List<BlockOccupancy>(){new BlockOccupancy(new Vector3i(0, 1, 0), typeof(PipeSlotBlock), new Quaternion(-0.7071068f, 0f, 0f, 0.7071068f), "ChimneyOut"),new BlockOccupancy(new Vector3i(0, 0, 0)),new BlockOccupancy(new Vector3i(1, 0, 0)),new BlockOccupancy(new Vector3i(1, 1, 0)), new BlockOccupancy(new Vector3i(2, 0, 0)),new BlockOccupancy(new Vector3i(2, 1, 0)),new BlockOccupancy(new Vector3i(3, 0, 0)),new BlockOccupancy(new Vector3i(3, 1, 0)),});
+                
+            /* Linked Recipes */
+            CraftingComponent.AddRecipe(typeof(NyElectricCementKilnObject), RecipeFamily.Get(typeof (ReinforcedConcreteRecipe)));
+            CraftingComponent.AddRecipe(typeof(NyElectricCementKilnObject), RecipeFamily.Get(typeof (CementRecipe)));
+            CraftingComponent.AddRecipe(typeof(NyElectricCementKilnObject), RecipeFamily.Get(typeof (MasonryAdvancedUpgradeRecipe)));
+        }
+
     }
-}
+
+
+    [Serialized]
+    [LocDisplayName("Electric Cement Kiln")]
+    [Ecopedia("Work Stations", "Craft Tables", createAsSubPage: true, display: InPageTooltip.DynamicTooltip)]
+    [LiquidProducer(typeof(SmogItem), 1)]
+    [AllowPluginModules(Tags = new[] { "AdvancedUpgrade" }, ItemTypes = new[] { typeof(MasonryAdvancedUpgradeItem) })]
+    public partial class NyElectricCementKilnItem : WorldObjectItem<NyElectricCementKilnObject>, IPersistentData
+    {
+        public override LocString DisplayDescription { get { return Localizer.DoStr("A superior replacement for the cement kiln that use electric power."); } }
+
+        static NyElectricCementKilnItem()
+        {
+
+        }
+
+        [TooltipChildren] public HomeFurnishingValue HousingTooltip { get { return HomeValue; } }
+        public static readonly HomeFurnishingValue HomeValue = new HomeFurnishingValue()
+        {
+            Category                 = RoomCategory.Industrial,
+            TypeForRoomLimit         = Localizer.DoStr(""),
+        };
+
+        [Tooltip(7)] private LocString PowerConsumptionTooltip => Localizer.Do($"Consumes: {Text.Info(3000)}w of {new HeatPower().Name} power from fuel");
+        [Serialized, TooltipChildren] public object PersistentData { get; set; }
+    }
+
+    [RequiresSkill(typeof(IndustrySkill), 0)]
+    public class NyElectricCementKilnRecipe : RecipeFamily
+    {
+        public NyElectricCementKilnRecipe()
+        {
+            this.Recipes = new List<Recipe>
+            {
+                new Recipe(
+                    "Electric Cement Kiln",
+                    Localizer.DoStr("Electric Cement Kiln"),
+                    new IngredientElement[]
+                    {
+                        new IngredientElement(typeof(AdvancedCircuitItem), 2, typeof(IndustrySkill)),
+                        new IngredientElement(typeof(SteelPlateItem), 24, typeof(IndustrySkill)),
+                        new IngredientElement(typeof(SteelPipeItem), 16, typeof(IndustrySkill)),
+                        new IngredientElement(typeof(BrickItem), 30, typeof(IndustrySkill)),
+                        new IngredientElement(typeof(SteelBarItem), 10, typeof(IndustrySkill)),
+                    },
+                    new CraftingElement<NyElectricCementKilnItem>()
+                )
+            };
+
+
+            this.ExperienceOnCraft = 7f;
+            this.LaborInCalories = CreateLaborInCaloriesValue(1000, typeof(IndustrySkill));
+            this.CraftMinutes = CreateCraftTimeValue(typeof(NyElectricCementKilnRecipe), 20, typeof(IndustrySkill));
+            this.Initialize(Localizer.DoStr("Electric Cement Kiln"), typeof(NyElectricCementKilnRecipe));
+
+            CraftingComponent.AddRecipe(typeof(ElectricMachinistTableObject), this);
+        }
+    }
 
 
 
